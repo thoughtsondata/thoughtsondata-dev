014543b4f855338f19c827c2fbe6c63d283b9962
Zarek <zarekbusiness@gmail.com>
2022-02-19 15:17:39 -0500

    redeploy

--- publishconf.py
+++ publishconf.py
@@ -10,10 +10,11 @@ from pelicanconf import *
 SITEURL = 'https://www.thoughtsondata.dev'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# FEED_ALL_ATOM = 'feeds/all.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = False
+# redeploy
 
 # Following items are often useful when publishing
 
