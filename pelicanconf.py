fbb286e6739421ac79639dac35eb0b65781e0f5c
Zarek <zarekbusiness@gmail.com>
2022-02-21 20:03:59 -0500

    Setting update

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,15 +29,25 @@ MANGLE_EMAILS = True
 
 
 # Blogroll
-LINKS = (('Index', 'https://thoughtsondata.dev/'),
-         ('About', 'https://thoughtsondata.dev/pages/about.html'),
+LINKS =     (("Index", "/index.html"),
+	("About", "/pages/about.html"),
          ('Mailing List', 'https://docs.google.com/forms/d/e/1FAIpQLSdpjM_LVVvRCKhuFiYqemRuycfRChsM-Pr3nIho5mDO7c9qOw/viewform?usp=sf_link'),)
+
+RELATIVE_URLS = True
 
 # Social widget
 SOCIAL = (('Twitter', 'https://twitter.com/thoughtsondata'),
           ('Email', 'mailto:hello@thoughtsondata.dev'),)
 
 DEFAULT_PAGINATION = 8
+
+ARCHIVES_URL = "archives.html"
+ARCHIVES_SAVE_AS = 'archives.html'
+ARTICLE_URL = 'articles/{slug}.html'
+ARTICLE_SAVE_AS = 'articles/{slug}.html'
+PAGE_URL = 'pages/{slug}.html'
+PAGE_SAVE_AS = 'pages/{slug}.html'
+TAGS_URL = 'tag/{slug}.html'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
